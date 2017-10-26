615228ad11b12450df5880e619cf288672af99fd
Laci Szűcs <ifrozen@gmail.com>
2017-10-26 10:33:14 +0200

    feat(JSONArray/parent): a sub-field type to select a "parent" ID

--- fields/types/jsonarray/JSONArrayField.js
+++ fields/types/jsonarray/JSONArrayField.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Field from '../Field';
-import uuid from 'uuid/v4';
+import uuidV4 from 'uuid/v4';
 import Collapsible from 'react-collapsible';
 import { map, set } from 'lodash';
 import Select from 'react-select';
 import { Glyph, Button, FormInput } from '../../../admin/client/App/elemental';
-
+const ROOT_PARENT_ID = 'ROOT';
 module.exports = Field.create({
 	displayName: 'JSONArrayField',
 	statics: {
@@ -15,25 +15,28 @@ module.exports = Field.create({
 		return <div>Rending da value {this.props.value}</div>;
 	},
 	renderField () {
+		this.sanitizeValue(this.props.value);
 		return (<div>
 			{(this.props.value || []).map((node, idx) => (
 				<Collapsible key={idx} trigger={this.renderNodeHeader(node)}>
 					{this.renderNode(idx, node)}
 				</Collapsible>
 			))}
-			<Button onClick={this.addNewItem}><Glyph name="plus" />Add new</Button>
+			<Button onClick={this.addNewItem}><Glyph name="plus" />&nbsp;Add new node</Button>
 		</div>);
 	},
 	renderNodeHeader (node) {
-		return (<h2>{node.question || 'Untitled'}</h2>);
+		return (<h2>{node.name || node.question || 'Untitled'}</h2>);
 	},
 	renderNode (idx, node) {
 		return (<div>
 			{map(this.props.jsonObjectSchema, (fieldOptions, fieldName) => (
 				<div key={`wrap-${idx}-${fieldName}`}>
-					<label htmlFor={this.getFormFieldName(idx, fieldName)}>
-						{fieldOptions.label || fieldName}
-					</label>
+					{fieldOptions.type !== 'id' && (
+						<label htmlFor={this.getFormFieldName(idx, fieldName)}>
+							{fieldOptions.label || fieldName}
+						</label>
+					)}
 					{this.renderSubField(idx, fieldName, fieldOptions, node)}
 				</div>
 			))}
@@ -43,6 +46,8 @@ module.exports = Field.create({
 		const name = this.getFormFieldName(idx, fieldName);
 		const key = `json-array-field-input-${idx}-${fieldName}`;
 		switch (fieldOptions.type) {
+			case 'id':
+				return <input type="hidden" name={name} key={key} value={node[fieldName]}/>;
 			case 'text':
 			case 'number':
 				return (
@@ -81,12 +86,49 @@ module.exports = Field.create({
 						onChange={newSubValue =>
 							this.valueChanged(idx, fieldName, newSubValue)}
 					/>);
-			case 'id':
-				return;
+			case 'parent':
+				const descendantIds = [node.id].concat(this.getDescendantsOf(node));
+				const rejectedIds = this.props.value.reduce((ids, otherNode) => {
+					if (descendantIds.includes(otherNode.id)) ids.push(otherNode.id);
+					return ids;
+				}, []);
+				const options = [];
+				const rootElement = node.parent === ROOT_PARENT_ID ? node :
+					this.props.value.find(node => node.parent === ROOT_PARENT_ID);
+				if (!rootElement || rootElement === node) {
+					options.push({
+						value: ROOT_PARENT_ID,
+						label: 'This item (root element)',
+					});
+				}
+				for (let { id, name: label } of this.props.value) {
+					if (!rejectedIds.includes(id)) {
+						options.push({ value: id, label });
+					}
+				}
+				console.log('options', options);
+				return (
+					<Select
+						key={key}
+						simpleValue
+						name={name}
+						value={node[fieldName]}
+						options={options}
+						onChange={newParent =>
+							this.valueChanged(idx, fieldName, newParent)}
+					/>);
 			default:
 				throw new Error(`Cannot render sub field ${fieldName} (type: ${fieldOptions.type})`)
-
 		}
+	},
+	getDescendantsOf (currentNode) {
+		return this.props.value.reduce((ids, otherNode) => {
+			if (otherNode.parent === currentNode.id) {
+				ids.push(otherNode.id);
+				ids = ids.concat(this.getDescendantsOf(otherNode));
+			}
+			return ids;
+		}, []);
 	},
 	getFormFieldName(idx, fieldName) {
 		return `${this.props.path}[${idx}][${fieldName}]`;
@@ -95,17 +137,29 @@ module.exports = Field.create({
 		const value = this.props.value.slice();
 		if (typeof value[idx] !== 'object') value[idx] = {};
 		set(value[idx], fieldName, newSubValue);
+		this.sanitizeValue(value);
 		this.props.onChange({
 			path: this.props.path,
 			value,
 		});
 	},
+	sanitizeValue(value) {
+		value.forEach(node => Object.assign(node, {
+			name: this.getName(node),
+		}));
+	},
 	addNewItem () {
 		this.props.onChange({
 			path: this.props.path,
-			value: this.props.value.concat([{
-				id: uuid(),
-			}]),
+			value: this.props.value.concat({
+				id: uuidV4(),
+				name: `New step`,
+				order: this.props.value.length + 1,
+			}),
 		});
+	},
+	getName (node) {
+		const parent = this.props.value.find(other => other.id === node.parent);
+		return `${parent && `${this.getName(parent)} - ` || ''}${node.question || 'Untitled'}`;
 	},
 });
