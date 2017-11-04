e12010576f72b2fdd6e860230b669b083a453f66
Laci Szűcs <ifrozen@gmail.com>
2017-11-04 12:52:43 +0100

    feat(JSONArray): clone node with or without descendants

--- fields/types/jsonarray/JSONArrayField.js
+++ fields/types/jsonarray/JSONArrayField.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Field from '../Field';
 import uuidV4 from 'uuid/v4';
 import Collapsible from 'react-collapsible';
-import { map, set, compact, startCase } from 'lodash';
+import { map, set, compact, max, startCase } from 'lodash';
 import Select from 'react-select';
 import { Glyph, Button, FormInput, FormLabel, Modal } from '../../../admin/client/App/elemental';
 const ROOT_PARENT_ID = 'ROOT';
@@ -56,13 +56,15 @@ module.exports = Field.create({
 		return <pre>{JSON.stringify(this.props.value, null, 2)}</pre>;
 	},
 	getInitialState: () => ({
-		nodeToRemove: null,
-		isConfirmRemoval: false
+		nodeToClone: null,
+
+		nodeToRemove: null
 	}),
 	renderField () {
 		const value = this.props.value && this.sanitizeValue(this.props.value);
 		return (<div>
-			{this.renderConfirmModal()}
+			{this.renderConfirmDeletionModal()}
+			{this.renderCloneModal()}
 			{(value || []).map((node, idx) => (
 				<Collapsible
 					className={css(classes.wholeFieldWrapper)}
@@ -74,10 +76,16 @@ module.exports = Field.create({
 					key={`json-array-node-${node.id}`}
 					trigger={this.renderNodeHeader(node)}>
 					{this.renderNode(idx, node)}
+					<Button color="success" onClick={() => this.onClickClone(node)}>
+						<Glyph name="repo-clone" />
+						&nbsp;
+						Clone this node
+					</Button>
+					&nbsp;
 					<Button color="danger" onClick={() => this.onClickDelete(node)}>
 						<Glyph name="trashcan" />
 						&nbsp;
-						Remove with children
+						Remove with descendants
 					</Button>
 				</Collapsible>
 			))}
@@ -181,7 +189,7 @@ module.exports = Field.create({
 				}
 				for (let node of this.props.value) {
 					if (node && !rejectedIds.includes(node.id)) {
-						options.push({ value: node.id, label: node.name });
+						options.push({ value: node.id, label: node.name || this.getName(node) });
 					}
 				}
 				return (
@@ -199,10 +207,10 @@ module.exports = Field.create({
 				throw new Error(`Cannot render sub field ${fieldName} (type: ${fieldOptions.type})`)
 		}
 	},
-	renderConfirmModal () {
+	renderConfirmDeletionModal () {
 		const nodeName = this.state.nodeToRemove && this.getName(this.state.nodeToRemove);
 		return (
-			<Modal.Dialog isOpen={this.state.isConfirmRemoval} onClose={this.handleCloseModal}>
+			<Modal.Dialog isOpen={!!this.state.nodeToRemove} onClose={this.handleCloseConfirmDeletionModal}>
 				<Modal.Header text={'Delete node and all its descendants?'} />
 				<Modal.Body>
 					This operation will remove "{nodeName}" and all its descendants.
@@ -217,7 +225,7 @@ module.exports = Field.create({
 					Are you sure?
 				</Modal.Body>
 				<Modal.Footer>
-					<Button color="warning" onClick={this.handleCloseModal}>
+					<Button color="warning" onClick={this.handleCloseConfirmDeletionModal}>
 						<Glyph name="x" />
 						&nbsp;
 						Cancel
@@ -226,24 +234,87 @@ module.exports = Field.create({
 					<Button color="danger" onClick={() => this.deleteItemWithDescendants(this.state.nodeToRemove)}>
 						<Glyph name="trashcan"/>
 						&nbsp;
-						Delete with descendants
+						Remove with descendants
 					</Button>
 				</Modal.Footer>
 
 			</Modal.Dialog>
 		);
 	},
-	onClickDelete (node) {
-		this.setState({
-			nodeToRemove: node,
-			isConfirmRemoval: true,
-		});
+	renderCloneModal () {
+		const nodeName = this.state.nodeToClone && this.getName(this.state.nodeToClone);
+		return (
+			<Modal.Dialog isOpen={!!this.state.nodeToClone} onClose={this.handleCloseCloneModal}>
+				<Modal.Header text={'Clone item'} />
+				<Modal.Body>
+					You are cloning "{nodeName}". Do you also want to clone all it's descendants?
+					The following children can also be cloned:
+					{nodeName && <ul>
+						{this.getDescendantsOf(this.state.nodeToClone).map(descendantId => (
+							<li>
+								{this.getName(this.props.value.find(({id}) => id === descendantId))}
+							</li>
+						))}
+					</ul>}
+					Do you want to clone all descendants or only this item?
+				</Modal.Body>
+				<Modal.Footer>
+					<Button color="warning" onClick={this.handleCloseCloneModal}>
+						<Glyph name="x" />
+						&nbsp;
+						Cancel
+					</Button>
+					&nbsp;
+					<Button color="primary" onClick={() => this.cloneItem(this.state.nodeToClone)}>
+						<Glyph name="dash" />
+						&nbsp;
+						Only clone this node
+					</Button>
+					&nbsp;
+					<Button color="primary" onClick={() => this.cloneItemWithDescendants(this.state.nodeToClone)}>
+						<Glyph name="list-unordered" />
+						&nbsp;
+						Clone with all descendants
+					</Button>
+				</Modal.Footer>
+			</Modal.Dialog>
+		);
 	},
-	handleCloseModal () {
-		this.setState({
-			nodeToRemove: null,
-			isConfirmRemoval: false
+	onClickClone (node) {
+		this.setState({ nodeToClone: node });
+	},
+	handleCloseCloneModal () {
+		this.setState({ nodeToClone: null });
+	},
+	onClickDelete (node) {
+		this.setState({ nodeToRemove: node });
+	},
+	handleCloseConfirmDeletionModal () {
+		this.setState({ nodeToRemove: null });
+	},
+	cloneItem (node) {
+		this.addNewItem(node, this.props.value.indexOf(node));
+	},
+	cloneItemWithDescendants (node) {
+		const sourceIds = [node.id].concat(this.getDescendantsOf(node));
+		const sources = this.props.value.filter(({ id }) => sourceIds.includes(id));
+		const afterIndex = max(sources.map(source => this.props.value.indexOf(source)));
+		const cloneMap = {};
+		const clones = sources.map(source => {
+			const id = cloneMap[source.id] = uuidV4();
+			return {
+				...source,
+				id,
+				sourceId: source.id,
+				question: `Copy of ${source.question}`,
+			};
+		}).map(clone => {
+			clone.parent = cloneMap[clone.parent] || clone.parent;
+			return clone;
 		});
+
+		this.addNewItems(clones, afterIndex);
+		this.handleCloseCloneModal();
 	},
 	deleteItemWithDescendants (node) {
 		const deleteIds = [node.id].concat(this.getDescendantsOf(node));
@@ -251,7 +322,7 @@ module.exports = Field.create({
 			path: this.props.path,
 			value: this.sanitizeValue(this.props.value.filter((node) => !deleteIds.includes(node.id || node._id))),
 		});
-		this.handleCloseModal();
+		this.handleCloseConfirmDeletionModal();
 	},
 	getDescendantsOf (currentNode) {
 		return this.props.value.reduce((ids, otherNode) => {
@@ -281,15 +352,34 @@ module.exports = Field.create({
 			});
 		}));
 	},
-	addNewItem () {
+	addNewItems (newItems, afterIndex) {
 		this.props.onChange({
 			path: this.props.path,
-			value: this.props.value.concat({
-				id: uuidV4(),
-				name: `New step`,
-				order: this.props.value.length + 1,
-			}),
+			value: [
+				...this.props.value.slice(0, afterIndex),
+				...newItems,
+				...this.props.value.slice(afterIndex + 1),
+			]
 		});
+
+	},
+	addNewItem (node = { name: `New step` }, afterIndex) {
+		const id = uuidV4();
+		const newItem = {
+			...node,
+			id,
+			order: this.props.value.length + 1,
+		};
+		this.props.onChange({
+			path: this.props.path,
+			value: afterIndex ? [
+				...this.props.value.slice(0, afterIndex),
+				newItem,
+				...this.props.value.slice(afterIndex + 1),
+			] : this.props.value.concat(newItem),
+		});
+		this.handleCloseCloneModal();
+		return id;
 	},
 	getName (node) {
 		node = node || {};
