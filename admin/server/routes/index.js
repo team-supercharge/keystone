3b0b082916b3bebb07645b646f1a923259bcb402
Adam Hurst <ahurst@Adams-MacBook-Pro.local>
2018-04-22 19:24:57 +0100

    fix(keystone): bind user role and home to keystone object

--- admin/server/routes/index.js
+++ admin/server/routes/index.js
@@ -38,6 +38,8 @@ module.exports = function IndexRoute (req, res) {
 		user: {
 			id: req.user.id,
 			name: UserList.getDocumentName(req.user) || '(no name)',
+			role: req.user.role,
+			home: req.user.home,
 		},
 		userList: UserList.key,
 		version: keystone.version,
