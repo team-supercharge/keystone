f2ca78ca8d262f3e3cc7b34e0bde5c28d392e51b
adamhurst <adam.j.hurst@gmail.com>
2018-04-26 16:01:04 +0100

    fix(introjs): tweaking the walkthrough text

--- admin/client/App/lmc/screens/Home/components/LmcTour.jsx
+++ admin/client/App/lmc/screens/Home/components/LmcTour.jsx
@@ -54,7 +54,7 @@ class LmcTour extends Component {
             .addStep({
                 element: document.querySelectorAll('#intro-js-step-tasks-info')[0],
                 intro: TOUR_TASKS_INFO,
-                position: 'left',
+                position: 'top',
             })
             .addStep({
                 element: document.querySelectorAll('#intro-js-step-advert')[0],
@@ -102,15 +102,15 @@ class LmcTour extends Component {
     }
 }
 
-const TOUR_HELLO = 'On the home page you\'ll see a snapshot of what has been happening over the last day and can perform some quick actions.​';
-const TOUR_RESIDENTS = 'Up here you\'ll see your active residents, these are those that are currently in your home.​ To start you need to add your residents and assign them to your care team. This then gets sent to the Carer App and allows the carers to record care on the go.​';
-const TOUR_ADD_RESIDENTS = 'The first step is to add your residents and assign them to your care team. This information then gets sent to the Carer App and allows carers to record care on the go.​'
+const TOUR_HELLO = 'On the home page you\'ll see a snapshot of what has been happening over the last day and can perform some quick actions.';
+const TOUR_RESIDENTS = 'Up here you\'ll see your active residents, these are those that are currently in your home';
+const TOUR_ADD_RESIDENTS = 'The first step is to add your residents and assign them to your care team. This information then gets sent to the Carer App and allows carers to record care on the go.'
 const TOUR_CARERS = 'Down here you can see who from your care team has been online and logged care today.';
 const TOUR_CARERS_INVITE = 'You can also invite other members of your team to join you in Log my Care.';
 const TOUR_INCIDENTS = 'Next is your incidents list. This gets populated when your carers record an incident log on the Carer App and lets you know what you need to deal with.';
 const TOUR_TASKS = 'The last section shows you the status of today’s To-Do\'s and Logs. This is where your scheduled care tasks and ad-hoc quick-logs recorded by your care team show up. ';
 const TOUR_TASKS_INFO = 'You can instantly see how many To-Do\'s are overdue, pending and have been completed today.';
-const TOUR_ADVERT = 'On the right you can see our tips and tricks videos to help you make the most of the Care Office along with our latest offers.';
+const TOUR_ADVERT = 'On the right you can see our tips and tricks videos to help you make the most of Log my Care along with our latest offers.';
 const TOUR_NAVBAR = 'Along the top you\'ll see menu options for all features, including how to access our help centre.';
 const TOUR_DONE = 'That\'s it - you\'ve finished the walkthrough of the Care Office!';
 
