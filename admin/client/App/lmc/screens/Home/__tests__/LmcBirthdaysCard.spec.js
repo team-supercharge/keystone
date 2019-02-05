971bdf24293204ed99feb8678c1daf5dd4dee9b2
Joel Baxter <joel@Joels-MacBook-Pro.local>
2019-02-05 10:50:09 +0000

    feat(CO-102): tests for correct birthday

--- admin/client/App/lmc/screens/Home/__tests__/LmcBirthdaysCard.spec.js
+++ admin/client/App/lmc/screens/Home/__tests__/LmcBirthdaysCard.spec.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import LmcBirthdaysCard from '../components/LmcBirthdaysCard'
+import MockDate from 'mockdate'
+
+MockDate.set('1/1/2019')
 
 describe('LmcBirthdaysCard', () => {
     let wrapper
     let residents
-    let mockDate
+    let birthday
 
     beforeEach(() => {
-        mockDate = new Date('01-07-1940')
+        birthday = new Date('01-07-1940')
         residents = [
             {
-                dateOfBirth: mockDate
+                id: 1,
+                dateOfBirth: birthday
             }
         ]
         wrapper = shallow((
@@ -23,5 +27,14 @@ describe('LmcBirthdaysCard', () => {
 
     test('should render correct snapshot', () => {
         expect(wrapper).toMatchSnapshot()
+    })
+
+    test('should display correct upcoming birthday', () => {
+        const birthdayAlert = wrapper.find('LmcResidentListItem')
+        expect(birthdayAlert.props().data.name).toEqual('79 years old on Monday 7th Jan')
+    })
+
+    afterAll(() => {
+        MockDate.reset()
     })
 })
