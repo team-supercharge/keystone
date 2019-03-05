7597bfe64665f69dbb87363e52f96ca8b6b28582
Joel Baxter <joel@Joels-MacBook-Pro.local>
2019-03-05 12:31:17 +0000

    feat(CO-122): registers browser as device

--- admin/client/utils/firebase.js
+++ admin/client/utils/firebase.js
@@ -1,5 +1,7 @@
 import firebase from 'firebase/app'
 import cloudMessaging from 'firebase/messaging'
+import xhr from 'xhr'
+import { browserName } from 'react-device-detect'
 
 const FIREBASE_CONFIG = {
     apiKey: "AIzaSyDrgw5aHG9His4a9x-xU9EhwkHtCsdQ80g",
@@ -11,7 +13,28 @@ const FIREBASE_CONFIG = {
 };
 
 const register = (token) => {
-    console.log(Keystone.user, token)
+    return new Promise((resolve, reject) => {
+        xhr({
+            url: `${Keystone.adminPath}/api/devices/register`,
+            method: 'POST',
+            headers: Object.assign({}, Keystone.csrf.header),
+            json: {
+                token,
+                deviceID: Keystone.user.id,
+                deviceType: 'Browser',
+                platform: browserName
+            }
+        }, (err, resp, body) => {
+            if (err || body && body.error) {
+                reject({
+                    message: body.error
+                })
+            } else {
+                resolve(resp)
+            }
+        })
+    })
+    
 }
 
 export default () => {
@@ -21,16 +44,15 @@ export default () => {
         messaging.usePublicVapidKey('BGGJ6DUZjryd06qgcEU-T1RsxbLK4cWVDLP7m4snIf0YUK6Iw3TvQtd359QNyqXxDU2A5juyrcWR7z23Sc-w75I');
 
         messaging.requestPermission()
-        .then(() => {
-            console.log('Got permission')
-            return messaging.getToken()
-        })
-        .then((token) => {
-            register(token)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+            .then(() => {
+                return messaging.getToken()
+            })
+            .then((token) => {
+                register(token)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
 
         messaging.onMessage((payload) => {
             console.log('onMessage', payload)
