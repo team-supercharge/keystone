c014fe6468df38c5e4e5e81b08c70c89ef08a48c
Joel Baxter <joel@Joels-MacBook-Pro.local>
2019-03-08 12:02:54 +0000

    fix(CO-126): removes daily logs and charts from profile

--- admin/client/App/lmc/screens/Residents/index.jsx
+++ admin/client/App/lmc/screens/Residents/index.jsx
@@ -127,8 +127,8 @@ export class LmcResidentsScreen extends Component {
 const navbarItems = [
     { label: 'Profile', url: 'profile', octicon: 'person' },
     // Removing these until these components are completed
-    { label: 'Daily Logs', url: 'daily-report', octicon: 'calendar' },
-    { label: 'Charts', url: 'charts', octicon: 'graph' },
+    // { label: 'Daily Logs', url: 'daily-report', octicon: 'calendar' },
+    // { label: 'Charts', url: 'charts', octicon: 'graph' },
     // { label: 'To-Dos', url: 'to-do', octicon: 'checklist' },
     { label: 'Documents', url: 'documents', octicon: 'file' }
 ]
