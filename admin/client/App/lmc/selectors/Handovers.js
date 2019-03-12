3795736aed270efa783ed5f320605fcd2967180b
Joel Baxter <joel@Joels-MacBook-Pro.local>
2019-03-12 10:04:39 +0000

    fix(CO-130): refactors handover selectors

--- admin/client/App/lmc/selectors/Handovers.js
+++ admin/client/App/lmc/selectors/Handovers.js
@@ -26,24 +26,8 @@ export const formatCurrentHandover = createSelector(
             !users) return null
 
         const residentsById = _.keyBy(residents, 'id')
-        const logsWithIcons = handover.logs.map(log => {
-            const item = _.find(categoryItems, { id: log.item })
-            const category = _.find(categories, { id: log.category })
-            const carer = _.find(users, { id: log.loggedBy })
-            return { 
-                ...log, 
-                itemIcon: { url: item.icon }, 
-                categoryColor: category.color,
-                carerName: `${carer.name.first} ${carer.name.last}`
-            }
-        })
-        const notesWithCarers = handover.notes.map(note => {
-            const carer = _.find(users, { id: note.createdBy })
-            return {
-                ...note,
-                carer
-            }
-        })
+        const logsWithIcons = getLogIcons(handover.logs, categoryItems, categories, users)
+        const notesWithCarers = getNoteCarers(handover.notes, users)
 
         return {
             logsByResident: _(logsWithIcons)
@@ -77,28 +61,9 @@ export const formatHandoverHistory = createSelector(
         const residentsById = _.keyBy(residents, 'id')
 
         const formattedHandovers = handovers.map(handover => {
-            const logsWithIcons = handover.logs.map(log => {
-                const item = _.find(categoryItems, { id: log.item })
-                const category = _.find(categories, { id: log.category })
-                const carer = _.find(users, { id: log.loggedBy })
-                return { 
-                    ...log, 
-                    itemIcon: { url: item.icon }, 
-                    categoryColor: category.color,
-                    carerName: `${carer.name.first} ${carer.name.last}`
-                }
-            })
-            const notesWithCarers = handover.notes.map(note => {
-                const carer = _.find(users, { id: note.createdBy })
-                return {
-                    ...note,
-                    carer
-                }
-            })
-            const seenByCarers = handover.seenBy.map(id => {
-                const carer = _.find(users, { id })
-                return carer
-            })
+            const logsWithIcons = getLogIcons(handover.logs, categoryItems, categories, users)
+            const notesWithCarers = getNoteCarers(handover.notes, users)
+            const seenByCarers = handover.seenBy.map(id => _.find(users, { id }))
 
             return {
                 logsByResident: _(logsWithIcons)
@@ -111,12 +76,39 @@ export const formatHandoverHistory = createSelector(
                     .value(),
                 notes: notesWithCarers,
                 seenBy: seenByCarers,
-                createdOn: handover.createdOn
+                createdOn: handover.createdOn,
+                createdBy: _.find(users, { id: handover.createdBy }),
+                witnessedBy: _.find(users, { id: handover.witnessedBy })
             }
         })
 
-        return _.groupBy(formattedHandovers, (handover) => {
+        const groupedHandovers = _.groupBy(formattedHandovers, (handover) => {
             return moment(handover.createdOn).startOf('day').format('MMM Do')
         })
+        return groupedHandovers
     }
 )
+
+const getLogIcons = (logs, categoryItems, categories, users) => {
+    return logs.map(log => {
+        const item = _.find(categoryItems, { id: log.item })
+        const category = _.find(categories, { id: log.category })
+        const carer = _.find(users, { id: log.loggedBy })
+        return { 
+            ...log, 
+            itemIcon: { url: item.icon }, 
+            categoryColor: category.color,
+            carerName: `${carer.name.first} ${carer.name.last}`
+        }
+    })
+}
+
+const getNoteCarers = (notes, users) => {
+    return notes.map(note => {
+        const carer = _.find(users, { id: note.createdBy })
+        return {
+            ...note,
+            carer
+        }
+    })
+}
